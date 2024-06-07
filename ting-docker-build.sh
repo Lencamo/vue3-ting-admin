a4a1b75779385c7884b58e31b0a1726b1bba1649
lencamo <207141481@qq.com>
2024-06-07 09:50:47 +0800

    fix: 更新ting-docker-build.sh

--- ting-docker-build.sh
+++ ting-docker-build.sh
@@ -4,4 +4,4 @@ yum install git -y
 git clone https://github.com/Lencamo/vue3-ting-admin.git
 cd ./vue3-ting-admin
 docker build -t ting-ui:v1.0 .
-docker run -itd --name ting-ui -p 81:80 ruoyi-ui:v1.0
+docker run -itd --name ting-ui -p 81:80 ting-ui:v1.0
