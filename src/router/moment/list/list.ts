b618a2e5e2111f545b773360b906121c31240897
lencamo <207141481@qq.com>
2024-06-14 14:59:52 +0800

    fix: 部署失败修复

--- src/router/moment/list/list.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  path: '/main/moment/list',
-  component: () => import('@/pages/moment/list/list.vue')
-}
--- src/router/moment/main/main.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  path: '/main/moment/main',
-  component: () => import('@/pages/moment/main/main.vue')
-}
